ec7e9640e340d59589edd8c11f7ce9747a44f702
Artem Golovanev <artemgolovanev@Artems-MacBook-Air.local>
2020-09-07 23:49:14 +0300

    MapController - almost done

--- InterfaceBuiderMapTestApp/AppDelegate.swift
+++ InterfaceBuiderMapTestApp/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GMSServices.provideAPIKey("AIzaSyAXsr1q7rwcUfRPaEmfrhvFe1wBjqBKCnA")
+        GMSPlacesClient.provideAPIKey("AIzaSyAXsr1q7rwcUfRPaEmfrhvFe1wBjqBKCnA")
         // Override point for customization after application launch.
         return true
     }
---
+++ InterfaceBuiderMapTestApp/CostView.swift
@@ -0,0 +1,14 @@
+//
+//  CostView.swift
+//  InterfaceBuiderMapTestApp
+//
+//  Created by Artem Golovanev on 06.09.2020.
+//  Copyright © 2020 Artem Golovanev. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+final class CostView: UIView {
+    
+}
